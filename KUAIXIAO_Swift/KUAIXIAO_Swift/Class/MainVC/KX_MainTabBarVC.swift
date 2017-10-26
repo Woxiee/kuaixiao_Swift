0f4ec1268a8f49191c286bd210fde68ad9991380
woxiee <97643020@qq.com>
2017-10-26 19:12:06 +0800

    2017-10-26
    上传图片资源文件

---
+++ KUAIXIAO_Swift/KUAIXIAO_Swift/Class/MainVC/KX_MainTabBarVC.swift
@@ -0,0 +1,23 @@
+//
+//  KX_MainTabBarVC.swift
+//  KUAIXIAO_Swift
+//
+//  Created by Macx on 2017/10/26.
+//  Copyright © 2017年 Faker. All rights reserved.
+//
+
+import UIKit
+
+class KX_MainTabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    func setup() {
+        
+        
+    }
+
+}
