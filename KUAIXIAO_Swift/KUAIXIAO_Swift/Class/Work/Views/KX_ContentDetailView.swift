dec430e8e84a4136cd755f20623c3c0b77870967
woxiee <97643020@qq.com>
2017-12-12 18:49:54 +0800

    2017-12-12
    更新约束

--- KUAIXIAO_Swift/KUAIXIAO_Swift/Class/Work/Views/KX_ContentDetailView.swift
+++ KUAIXIAO_Swift/KUAIXIAO_Swift/Class/Work/Views/KX_ContentDetailView.swift
@@ -34,36 +34,36 @@ class KX_ContentDetailView: UIView {
     
     func setUpUI(){
         
-        var lastView : Any  = self
-        let margin:CGFloat = 10
-        for i in 0...2 {
-           let  titlelB = UILabel()
-                titlelB.font = font13
-                self.addSubview(titlelB)
-
-            let contenlb = UILabel()
-               contenlb.font = font13
-            self.addSubview(contenlb)
-
-            titlelB.whc_Left(0).whc_Top(0, toView: lastView as? UIView ).whc_Right(0, toView: self).whc_Height(margin*2)
-            
-            contenlb.whc_Left(0).whc_Top(margin - 4, toView: titlelB).whc_Right(0, toView: self).whc_HeightAuto()
- 
-            titlelB.text = "今日计划"
-            contenlb.text = "测试数据"
-            lastView = contenlb as? UILabel
-
-         
-        }
-//        titleLb.font = font13
-//        self.addSubview(titleLb)
+//        var lastView : Any  = self
+//        let margin:CGFloat = 10
+//        for i in 0...2 {
+//           let  titlelB = UILabel()
+//                titlelB.font = font13
+//                self.addSubview(titlelB)
+//
+//            let contenlb = UILabel()
+//               contenlb.font = font13
+//            self.addSubview(contenlb)
+//
+//            titlelB.whc_Left(0).whc_Top(0, toView: lastView as? UIView ).whc_Right(0, toView: self).whc_Height(margin*2)
+//
+//            contenlb.whc_Left(0).whc_Top(margin - 4, toView: titlelB).whc_Right(0, toView: self).whc_HeightAuto()
+//
+//            titlelB.text = "今日计划"
+//            contenlb.text = "测试数据"
+//            lastView = contenlb as? UILabel
 //
 //
-//        contenLB.font = font15
-//        self.addSubview(contenLB)
-//
-//        titleLb.whc_Left(0).whc_Top(0, toView: self).whc_Right(0, toView: self).whc_Height(18)
-//        contenLB.whc_Left(0).whc_Top(8, toView: titleLb).whc_Right(0, toView: self).whc_HeightAuto().whc_Bottom(10)
+//        }
+        titleLb.font = font13
+        self.addSubview(titleLb)
+
+
+        contenLB.font = font15
+        self.addSubview(contenLB)
+
+        titleLb.whc_Left(0).whc_Top(0, toView: self).whc_Right(0, toView: self).whc_Height(18)
+        contenLB.whc_Left(0).whc_Top(8, toView: titleLb).whc_Right(0, toView: self).whc_HeightAuto().whc_Bottom(10)
     }
 
     
